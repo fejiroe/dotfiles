470be15fd7983029f5238578a581f3a3630660ff
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2025-07-13 21:14:46 +0100

    ttatetjlktj

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,7 +15,7 @@ vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 vim.o.undofile = true
 
 vim.schedule(function()
-    vim.o.clipboard = 'unnamedplus'
+	vim.o.clipboard = 'unnamedplus'
 end)
 
 vim.g.have_nerd_font = true
@@ -33,138 +33,177 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 --  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.hl.on_yank()
-    end,
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
 })
+
+
+-- PLUGINS --
+
+
+local function scstatus()
+	if vim.bo.filetype == "supercollider" then
+		stat = vim.fn["scnvim#statusline#server_status"]()
+		stat = stat:gsub("%%", "♪")
+		return stat
+	else
+		return ""
+	end
+end
 
 -- lazy pm
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = {
-        -- add your plugins here
-        'tpope/vim-fugitive',
-        'nvim-treesitter/nvim-treesitter',
-        'nvim-lualine/lualine.nvim',
-        "xzbdmw/colorful-menu.nvim",
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
-    ui = {
-        border = "single",
-        size = {
-            width = 0.8,
-            height = 0.8,
-        },
-    },
-    checker = { enabled = true },
-})
+	spec = {
+		-- add plugins
+		'tpope/vim-fugitive',
+		'nvim-treesitter/nvim-treesitter',
+		'nvim-lualine/lualine.nvim',
+		"xzbdmw/colorful-menu.nvim",
+		'davidgranstrom/scnvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+		},
+		ui = {
+			border = "single",
+			size = {
+				width = 0.81,
+				height = 0.81,
+			},
+		},
+		checker = { enabled = true },
+	})
 
--- treesitter
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "asm", "c", "cpp", "lua", "vim", "vimdoc",  "markdown", "markdown_inline", "swift", "rust", "zig" },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-}
+	-- treesitter
+	require'nvim-treesitter.configs'.setup {
+		ensure_installed = { "asm", "c", "cpp", "lua", "vim", "vimdoc",  "markdown", "markdown_inline", "swift", "rust", "zig" },
+		sync_install = false,
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+	}
 
-vim.lsp.enable('treesitter')
+	vim.lsp.enable('treesitter')
 
--- lua line
-local colors = {
-    color0 = "#092236",
-    color1 = "#ff5874",
-    color2 = "#c3ccdc",
-    color3 = "#1c1e26",
-    color6 = "#a1aab8",
-    color7 = "#828697",
-    color8 = "#ae81ff",
-}
+	-- lua line
+	local colors = {
+		color0 = "#092236",
+		color1 = "#ff5874",
+		color2 = "#c3ccdc",
+		color3 = "#1c1e26",
+		color6 = "#a1aab8",
+		color7 = "#828697",
+		color8 = "#ae81ff",
+	}
 
-require('lualine').setup {
-    options = {
-        icons_enabled = true,
-        theme = '16color',
-        component_separators = { left = '|', right = ''},
-        section_separators = { left = '|', right = ''},
-        disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-        },
-        ignore_focus = {},
-        always_divide_middle = true,
-        always_show_tabline = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-            refresh_time = 8, -- ~120fps
-            events = {
-                'WinEnter',
-                'BufEnter',
-                'BufWritePost',
-                'SessionLoadPost',
-                'FileChangedShellPost',
-                'VimResized',
-                'Filetype',
-                'CursorMoved',
-                'CursorMovedI',
-                'ModeChanged',
+	require('lualine').setup {
+		options = {
+			icons_enabled = true,
+			theme = '16color',
+			component_separators = { left = '|', right = ''},
+			section_separators = { left = '|', right = ''},
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
+			},
+			ignore_focus = {},
+			always_divide_middle = true,
+			always_show_tabline = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
+				refresh_time = 8, -- ~120fps
+				events = {
+					'WinEnter',
+					'BufEnter',
+					'BufWritePost',
+					'SessionLoadPost',
+					'FileChangedShellPost',
+					'VimResized',
+					'Filetype',
+					'CursorMoved',
+					'CursorMovedI',
+					'ModeChanged',
+				},
+			}
+		},
+		sections = {
+			lualine_a = {'mode'},
+			lualine_b = {'branch', 'diff', 'diagnostics'},
+			lualine_c = {'filename', scstatus},
+			lualine_x = {'filetype'},
+			lualine_y = {'progress'},
+			lualine_z = {'location'}
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = {'filename'},
+			lualine_x = {'location'},
+			lualine_y = {},
+			lualine_z = {}
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {}
+	}
+
+        -- supercollider
+        local scnvim = require 'scnvim'
+        local map = scnvim.map
+        local map_expr = scnvim.map_expr
+        scnvim.setup {
+            -- config here
+            keymaps = {
+                ['<M-e>'] = map('editor.send_line', {'i', 'n'}),
+                ['<C-e>'] = {
+                    map('editor.send_block', {'i', 'n'}),
+                    map('editor.send_selection', 'x'),
+                },
+                ['<CR>'] = map('postwin.toggle'),
+                ['<M-CR>'] = map('postwin.toggle', 'i'),
+                ['<M-L>'] = map('postwin.clear', {'n', 'i'}),
+                ['<C-k>'] = map('signature.show', {'n', 'i'}),
+                ['<F12>'] = map('sclang.hard_stop', {'n', 'x', 'i'}),
+                ['<leader>st'] = map(scnvim.start),
+                ['<leader>sk'] = map(scnvim.recompile),
+                ['<F1>'] = map_expr('s.boot'),
+                ['<F2>'] = map_expr('s.meter'),
             },
         }
-    },
-    sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-    },
-    inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
-    },
-    tabline = {},
-    winbar = {},
-    inactive_winbar = {},
-    extensions = {}
-}
+        vim.g.scnvim_postwin_syntax_hl = 1
 
--- transparent background
-vim.cmd [[
-highlight Normal guibg=none
-highlight NonText guibg=none
-highlight Normal ctermbg=none
-highlight NonText ctermbg=none
-]]
+	-- transparent background
+	vim.cmd [[
+	highlight Normal guibg=none
+	highlight NonText guibg=none
+	highlight Normal ctermbg=none
+	highlight NonText ctermbg=none
+	]]
 
--- keymaps
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
+	-- keymaps
+	vim.g.mapleader = " "
+	vim.g.maplocalleader = "\\"
 
