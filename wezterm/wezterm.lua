fce8f5f5f811e539e6d7ab9b4a3c89ff1b9aa713
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2025-10-18 01:56:14 +0100

    tmux config finally

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,8 +9,13 @@ config.window_background_opacity = 0.33
 config.macos_window_background_blur = 21
 config.window_decorations = "RESIZE | MACOS_FORCE_DISABLE_SHADOW | MACOS_FORCE_SQUARE_CORNERS"
 config.hide_tab_bar_if_only_one_tab = true
-config.cursor_blink_rate = 2
+config.cursor_blink_rate = 21
 cursor_blink_ease_in = "Linear"
 cursor_blink_ease_out = "Linear"
-
+config.window_padding = {
+    left = 3,
+    right = 1,
+    top = 0,
+    bottom = 6,
+}
 return config
